26ae86114f24f9e6f31f8ff1a130acf36612d7ef
AnishRM-ai <anishmagar228@gmail.com>
2025-08-07 13:19:08 +0545

    Completed authentication api.

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,16 +1,15 @@
 import express from "express";
-
+import { signup, login, logout, updateProfile, checkAuth } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
-router.post("/signup", (req, res)=>{
-    res.send("signup route")
-});
+router.post("/signup", signup);
 
-router.post("/login", (req, res)=>{
-    res.send("login route")
-});
+router.post("/login", login);
 
-router.get("/logout", (req, res)=>{
-    res.send("logout route")
-});
+router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 export default router;
